f9d47bd588f0baa41f7d653bd1ec37c9998bde2f
Dawid Roszman <dawidroszman@protonmail.com>
2024-05-21 14:43:26 +0200

    created docker-compose

--- cinema-frontend/src/utils/apiAddress.ts
+++ cinema-frontend/src/utils/apiAddress.ts
@@ -1,3 +1,5 @@
-export const api = "http://localhost:8080";
+import process from "process";
+export const api = process.env.NEXT_PUBLIC_API;
+// export const api = "http://localhost:8080";
 //export const api = "http://pi.dawidroszman.eu:8080";
 // export const api = "https://ab14-85-237-187-72.ngrok-free.app";
